14eefaa0c60fb2fc52285f27419f0e50f2965773
gabrielrachieru <rachieru.gabriel2016@gmail.com>
2025-07-10 14:36:46 +0300

    .

--- LeetCode/21/Solution.java
+++ LeetCode/21/Solution.java
@@ -10,6 +10,26 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-
+        ListNode c1 = list1;
+        ListNode c2 = list2;
+        ListNode res = new ListNode();
+        ListNode cur = res;
+        while (c1 != null && c2 != null){
+            if(c1.val < c2.val){
+                cur.next = c1;
+                c1 = c1.next;
+            }
+            else if(c1.val >= c2.val){
+                cur.next = c2;
+                c2 = c2.next;
+            }
+            cur = cur.next;
+        }
+        if (c1 != null) {
+            cur.next = c1;
+        } else {
+            cur.next = c2;
+        }
+        return res.next;
     }
 }
